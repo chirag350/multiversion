0f4fc036208520b9167e05e8f25a4f4f44254890
Dviih <50752447+Dviih@users.noreply.github.com>
2021-05-02 18:28:04 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@
 cd /home/container
 echo "Multi Version Beta"
 echo "By Dviih (dviih.technology/multiversion)"
-/java/bin/java -version
+java -version
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 ${MODIFIED_STARTUP}
