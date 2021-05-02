e1a0fc24b857289d49a71e8d91ab2cc5e4832664
Dviih <50752447+Dviih@users.noreply.github.com>
2021-05-02 19:36:08 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,5 @@ echo "By Dviih (dviih.technology/multiversion)"
 java -version
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
+echo "eula=true" > eula.txt
 ${MODIFIED_STARTUP}
