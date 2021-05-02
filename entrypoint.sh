a82021155c47c0a20b0ded772e74d65176b5e5cc
Dviih <50752447+Dviih@users.noreply.github.com>
2021-05-02 07:44:51 -0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+# Multi Versin Entrypoint.sh
+# By Dviih
+# dviih.technology/multiversion
+
+#!/bin/bash
+cd /home/container
+echo "Multi Version Beta"
+echo "By Dviih (dviih.technology/multiversion)"
+/java/bin/java -version
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+${MODIFIED_STARTUP}
