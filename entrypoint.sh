621da404d6aeca49cc48732a1ad7bfd72a08c10e
Dviih <50752447+Dviih@users.noreply.github.com>
2021-05-17 08:31:31 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,14 @@
 # By Dviih
 # dviih.technology/multiversion | dviih@dviih.email
 
-#!/bin/sh
+#!/bin/bash
 cd /home/container
 echo "MultiVersion"
 echo "By Dviih (dviih.technology/multiversion)"
+if [[ -f "$SERVER_JARFILE" ]]
+then
+    echo "You don't have a $SERVER_JARFILE please select a version on startup tab then reinstall the server."
+fi
 java -version
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
