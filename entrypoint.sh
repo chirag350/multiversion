79dd3877c8703b90011c7b5d75d3e2215355e753
Dviih <50752447+Dviih@users.noreply.github.com>
2021-06-14 20:10:48 -0300

    Removed check version since wizard is coming

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,6 @@
 #!/bin/bash
 cd /home/container
 echo "MultiVersion | dviih.technology/multiversion"
-if [[ -f "$SERVER_JARFILE" ]]
-then
-    echo "You don't have a $SERVER_JARFILE please select a version on startup tab then reinstall the server."
-fi
 java -version
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
