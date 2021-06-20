d54879b5bc72d34641172a1b5a808bf26b03e588
Chirag The Doge <72273879+chirag350@users.noreply.github.com>
2021-06-21 03:12:35 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,18 @@
 # MultiVersion | dviih.technology/multiversion | dviih@dviih.email
 
 #!/bin/bash
-cd /home/container
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
+export INTERNAL_IP
+
+cd /home/container || exit 1 # exit if /home/container doesnt exist
 echo "MultiVersion | dviih.technology/multiversion"
 java -version
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
-echo "eula=true" > eula.txt
+echo "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n $MODIFIED_STARTUP"
+echo "eula=true" | tee eula.txt
 ${MODIFIED_STARTUP}
