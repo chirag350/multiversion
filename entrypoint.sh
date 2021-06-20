57cdf4303a9e6dec6c69b161a6052cf7c084b563
Chirag The Doge <72273879+chirag350@users.noreply.github.com>
2021-06-21 03:14:29 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ export INTERNAL_IP
 cd /home/container || exit 1 # exit if /home/container doesnt exist
 echo "MultiVersion | dviih.technology/multiversion"
 java -version
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 echo "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n $MODIFIED_STARTUP"
-echo "eula=true" | tee eula.txt
+echo "eula=true" | tee eula.txt > /dev/null
 ${MODIFIED_STARTUP}
